7825c2d0d051e2d2f9678131323ad30abec563c6
vincepr <vinceprinceking@gmail.com>
2023-07-06 09:03:52 +0200

    testing printError

--- srcweb/load_emscripten.js
+++ srcweb/load_emscripten.js
@@ -7,6 +7,24 @@ var Module = {
   
   postRun: [],
 
+  printError: (function(){
+    var element = document.getElementById('output');
+    if (element) element.value = ''; // clear browser cache
+    return function(text) {
+      if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+      // These replacements are necessary if you render to raw HTML
+      //text = text.replace(/&/g, "&amp;");
+      //text = text.replace(/</g, "&lt;");
+      //text = text.replace(/>/g, "&gt;");
+      //text = text.replace('\n', '<br>', 'g');
+      console.log(text);
+      if (element) {
+        element.value += text + "\n";
+        element.scrollTop = element.scrollHeight; // focus on bottom
+      }
+    };
+  })(),
+
   print: (function() {
     var element = document.getElementById('output');
     if (element) element.value = ''; // clear browser cache
