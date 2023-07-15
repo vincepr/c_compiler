bb7ed9f1738ca947a08340c09a389c67e0718543
vincepr <vincepr@gmx.de>
2023-07-15 17:46:43 +0200

    hooked up my own python test-suite

--- tests/main.py
+++ tests/main.py
@@ -13,6 +13,42 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def getAllLoxFiles(loxbinary, pathToTests):
+    return glob(pathToTests, recursive=True)
+
+
+
+# executes file and checks for expected output marked with "expect: ....."
+# for errors we just get them and search if they are referenced anywhere in the file -> then were fine
+def testFile(loxbinary, filepath):
+    result = subprocess.run([loxbinary, filepath], capture_output=True, universal_newlines = True )
+    outLines = result.stdout.splitlines()
+    errLines = result.stderr.splitlines()
+    with open(filepath) as f:
+            idx = 0 # line-nr
+            FAILED = F"{bcolors.FAIL}FAILED:{bcolors.ENDC}"
+            PATHTESTED = F"{bcolors.WARNING}{loxbinary} {filepath}{bcolors.ENDC}"
+            for line in f:
+                idx+=1
+                for err in errLines:
+                    if err in line: errLines.remove(err) #  we just see if our errors get mentioned somewhere
+                if "expect: " in line:                  # check if we expect an std-output in this line?
+                    if len(outLines) <=0:
+                        print(F"{FAILED} {PATHTESTED} @line[{idx}] '{line.strip()}'")
+                        return False
+                    if "expect: "+outLines[0] in line :
+                        outLines.pop(0) # we matched this output
+                    else:
+                        print(F"{FAILED} {PATHTESTED} @line[{idx}] '{line}' got: '{outLines[0]}'")
+                        return False
+            ## we ran trough all lines check if we matched all errors and output from our cmd
+            if len(outLines)!=0 and len(errLines)!=0:
+                print(F"{FAILED} {PATHTESTED} to many Errors or std-Output\n")
+                return False
+            else: return True
+
+            
+
 
 def unitTestFile(loxbinary, filepath):
 
@@ -22,23 +58,22 @@ def unitTestFile(loxbinary, filepath):
             for line in f:
                 current = line.strip()
                 if (len(outLines)<=0):
-                    print ("sucess: " + filepath)
+                    #print ("sucess: " + filepath)
                     return True
                 if ("expect: " +outLines[0]) in current:
                     #print("found: '"+ ("expect: " +outLines[0])+ "' == " + current)
                     outLines.pop(0)
 
-        print(bcolors.FAIL +"FAILED: for file: "+ filepath +bcolors.ENDC)
+        print(bcolors.FAIL +"FAILED: " + loxbinary + " " + filepath +bcolors.ENDC)
         return False
     # get all lines our *.lox file outputs
+    #print(F"starting file: {filepath}")
     result = subprocess.run([loxbinary, filepath], capture_output=True, universal_newlines = True )
     outLines = result.stdout.splitlines() + result.stderr.splitlines()
     return checkExpectedOut(filepath, outLines)
 
-def getAllLoxFiles(loxbinary, pathToTests):
-    return glob(pathToTests, recursive=True)
 
-
+## our main process:
 loxbinary = sys.argv[1] #"./binary.out"
 pathTestFiles = sys.argv[2].rstrip("/")+"/**/*.lox" #"./tests" + 
 if len(sys.argv) != 3:
@@ -47,13 +82,12 @@ if len(sys.argv) != 3:
 files = getAllLoxFiles(loxbinary, pathTestFiles)
 print(F"found {len(files)} *.lox-files. Starting testing...")
 
-good = 0
 bad = 0
 for file in files:
-    didSucceed = unitTestFile(loxbinary,file)
-    if didSucceed: good += 1
-    else: bad +=1
-print (F"Finished {len(files)} tests: {bcolors.OKGREEN}passed: {good}{bcolors.ENDC}, {bcolors.FAIL}failed: {bad}{bcolors.ENDC}")
+    didSucceed = testFile(loxbinary,file)
+    if not didSucceed: bad += 1
+
+print (F"{bcolors.HEADER}Finished {len(files)} tests: {bcolors.ENDC} {bcolors.OKGREEN}passed: {len(files)-bad}{bcolors.ENDC}, {bcolors.FAIL}failed: {bad}{bcolors.ENDC}")
 
 if bad != 0: exit(1)
 else: exit(0)
